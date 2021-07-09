6a5a3beec1351f34256f58c1e32ae4b9fecc752f
lazyman2775 <82196273+lazyman2775@users.noreply.github.com>
2021-07-09 21:28:55 +0900

    ㅇㅇㅇ
    처음 올리는 거당

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public float turnSpeed = 10;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, turnSpeed * Time.deltaTime);
+
+    }
+}
